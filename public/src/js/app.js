ffdeccbb0c9e74eed096a40b15da930a3b197068
Joao Paulo Lethier <jplethier@gmail.com>
2020-07-08 10:18:38 -0300

    add comments with ajax and fetch examples for reference

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,6 +5,9 @@ if ('serviceWorker' in navigator) {
     .register('/sw.js')
     .then(function() {
       console.log('service worker registered');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
@@ -14,3 +17,45 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 })
+
+// ajax and fetch using examples for reference
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://httpbin.org/ip');
+// xhr.responseType = 'json';
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// };
+
+// xhr.onerror = function() {
+//   console.log('Error!');
+// };
+
+// xhr.send();
+
+// fetch('https://httpbin.org/ip').then(function(response) {
+//   console.log(response);
+//   return response.json();
+// }).then(function(body) {
+//   console.log(body);
+// }).catch(function(err) {
+//   console.log(err);
+// });
+
+// fetch('https://httpbin.org/post', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json'
+//   },
+//   mode: 'cors',
+//   body: JSON.stringify({
+//     message: 'Does this work?'
+//   })
+// }).then(function(response) {
+//   console.log(response);
+//   return response.json();
+// }).then(function(body) {
+//   console.log(body);
+// }).catch(function(err) {
+//   console.log(err);
+// });
