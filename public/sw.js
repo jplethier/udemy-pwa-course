a7ae0c37fc73ccf17f2dc44bb81c51ac4392eb70
Joao Paulo Lethier <jplethier@gmail.com>
2020-10-25 17:13:52 -0300

    simple offline fallback with cache;

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = 'static-v5';
+var CACHE_STATIC_NAME = 'static-v6';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 
 self.addEventListener('install', function(event) {
@@ -18,6 +18,7 @@ self.addEventListener('install', function(event) {
           'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
           'https://fonts.googleapis.com/css?family=Roboto:400,700',
           'https://fonts.googleapis.com/icon?family=Material+Icons',
+          '/offline.html'
         ])
       })
   )
@@ -53,11 +54,14 @@ self.addEventListener('fetch', function(event) {
               return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache) {
                   // response just can be used once, so it is important to use response.clone
-                  // cache.put(event.request.url, res.clone());
+                  cache.put(event.request.url, res.clone());
                   return res;
                 })
             }).catch(function(err) {
-
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function(cache) {
+                  return cache.match('/offline.html')
+                })
             });
         }
       })
