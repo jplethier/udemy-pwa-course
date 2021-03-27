2560720c495b382121522ddce7c86103e16bec49
Joao Paulo Lethier <jplethier@gmail.com>
2021-03-27 18:36:05 -0300

    reacting to notification click

--- public/sw.js
+++ public/sw.js
@@ -153,6 +153,23 @@ self.addEventListener('notificationclick', function(event) {
 
   if (action === 'confirm') {
     notification.close();
+  } else {
+    event.waitUntil(
+      clients.matchAll()
+        .then(function(clis) {
+          var client = clis.find(function(c) {
+            return c.visibilityState === 'visible';
+          })
+
+          if (client !== undefined) {
+            client.navigate('http://localhost:8080');
+            client.focus();
+          } else {
+            clients.openWindow('http://localhost:8080');
+          }
+        })
+    )
+    notification.close();
   }
 });
 
